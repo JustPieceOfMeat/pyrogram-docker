5ed1169f9243617517ce883b6d523ce9206ccedd
Alexey Razumov <alexey_razumov@tutanota.com>
2021-02-27 16:41:23 +0300

    added tgcrypto

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install https://github.com/pyrogram/pyrogram/archive/master.zip
+pip install https://github.com/pyrogram/pyrogram/archive/master.zip tgcrypto
 
 if test -f "requirements.txt"; then
     pip install -r requirements.txt
